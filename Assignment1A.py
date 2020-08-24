1c96207b0b685b4f7e80595dde481c5535c76873
Jungsup Kim <kim8299270@gmail.com>
2020-08-24 23:22:28 +1200

    conditiona_prob
    conditiona_prob

--- Assignment1A.py
+++ Assignment1A.py
@@ -22,6 +22,13 @@ print(rawdata)
 predictors = rawdata.iloc[:,1:ncol]
 target = rawdata.iloc[:,0:1]
 
+def get_conditional_prop(max,num_of_class,total,prem):
+    denominator = num_of_class / total
+    numerator = denominator * prem
+    conditional_prop = max * (numerator / denominator)
+
+    return conditional_prop
+
 pred_train, pred_test, tar_train, tar_test = train_test_split(predictors,target,test_size=0.3,random_state=0,stratify=target)
 split_threshold = 3
 for i in range(2, split_threshold):
@@ -41,12 +48,13 @@ for i in range(2, split_threshold):
     recall = recall_score(y_true=tar_test, y_pred=predictions, average='micro')
     print("Recall score of our model with Decision Tree :", recall)
     tree.plot_tree(classifier, filled=True)
-    prob = classifier.predict_proba(pred_test)
-    print(prob)    #get the probability of each sample.
+    prob_tree = classifier.predict_proba(pred_test)
+    print(prob_tree)    #get the probability of each sample.
+
 
 #MLP Classification
 
-clf = MLPClassifier(activation = 'logistic', solver="sgd", learning_rate_init = 0.1, alpha= 0.000005, hidden_layer_sizes=(5,2),
+clf = MLPClassifier(activation = 'logistic', solver="sgd", learning_rate_init = 0.1, alpha= 0.000005, hidden_layer_sizes=(28,3),
                     random_state=1, max_iter=500)
 clf.fit(pred_train,np.ravel(tar_train, order="C"))
 predictions_mlp = clf.predict(pred_test)
@@ -55,6 +63,82 @@ prob_mlp = clf.predict_proba(pred_test)
 print(prob_mlp)
 #scores = cross_val_score(clf, predictions_mlp, target, cv=10)
 #print("Accuracy score of our model with MLP under cross validation", scores.mean())
+
+confusion_mlp = confusion_matrix(tar_test, predictions_mlp)
+
+print(confusion_mlp)
+
+save_class = []
+
+def check_index(highest_index):
+    if highest_index == 0:
+        save_class.append(['d '])
+    elif highest_index == 1:
+        save_class.append(['h '])
+    elif highest_index == 2:
+        save_class.append(['o '])
+    elif highest_index == 3:
+        save_class.append(['s '])
+
+def check_prem(index):
+    if index == 0:
+        return [0.86,48]
+    elif index == 1:
+        return [0.80,26]
+    elif index == 2:
+        return [0.81,25]
+    elif index == 3:
+        return [0.84,58]
+
+
+for i in range(0,len(prob_mlp)):
+    mlp_list = prob_mlp[i]
+    tree_list = prob_tree[i]
+    test = np.add(mlp_list,tree_list)/2
+    print("==================")
+    highest_index = max(test)
+    print(test)
+    print(highest_index)
+    #check_list = isinstance(test, dict)  0.3137
+    highest_index = list(test).index(highest_index)
+    check_index(highest_index)
+
+
+print(save_class)
+print(np.array(tar_test))
+print("Accuracy score of our model with MLP:", accuracy_score(tar_test,save_class))
+print(prob_mlp)
+total_class_dt = 0
+prem_dt = 0
+
+for i in range(0, len(prob_mlp)):
+    #print(tree_list_con)
+    mlp_list = list(prob_tree[i])
+
+    print("prop_tree")
+    max_prop = (max(prob_tree[i]))
+    print(max_prop)
+    index = mlp_list.index(max_prop)
+    #get_conditional_prop()
+    print(index)
+    premision = check_prem(index)
+    total_class_dt = premision[1]
+    prem_dt = premision[0]
+    print(total_class_dt)
+    print(prem_dt)
+    print(max_prop)
+
+
+    result = get_conditional_prop(max_prop,total_class_dt,157,prem_dt)
+    print(result)
+
+
+
+
+
+
+
+#get_conditional_prop(max,num_of_class,total,prem)
 
 
 
